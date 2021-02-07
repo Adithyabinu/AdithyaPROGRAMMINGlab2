96309c8adecebdaff71cbeb8f29d00c8c65e5ba9
Adithyabinu <77437806+Adithyabinu@users.noreply.github.com>
2021-02-07 14:56:24 +0530

    Add files via upload

---
+++ CO1programe18.py
@@ -0,0 +1,12 @@
+d1 = {'Adithya': 'Bibithya',
+      'Athira': 'Nimisha',
+      'Sangeetha': 'Sanchana',
+      'Aswathy':'sreelakshmi',
+      'Arya':'Anjana'}
+d2 = {'Amalu': 'Arundhady',
+      'Bhavya': 'Ammu',
+      'Harsha':'Dilsha',
+      'Krishnaindu':'Akshaya',
+      'Reshma':'Roshna'}
+d3 = {**d1, **d2}
+print(d3)
