e923b756dcf65cd223d466d178d543b13d2079b4
Adithyabinu <77437806+Adithyabinu@users.noreply.github.com>
2021-02-08 21:54:21 +0530

    Add files via upload

---
+++ CO1programe7a.py
@@ -0,0 +1,8 @@
+list1=[11,22,33,56,78]
+list2=[1,4,6,7,8]
+print("the first list is:"+str(list1))
+print("the second list is:"+str(list2))
+if len(list1)==len(list2):
+    print("Length of the list are same")
+else:
+        print("length of the list are not same")
