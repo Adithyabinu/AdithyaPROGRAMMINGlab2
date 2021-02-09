cd2d223e300785e608cc06490428195379d404b1
Adithyabinu <77437806+Adithyabinu@users.noreply.github.com>
2021-02-09 11:30:21 +0530

    Add files via upload

---
+++ CO1programe8.py
@@ -0,0 +1,9 @@
+w=str(input("enter data:"))
+for i in range(0,len(w)):
+    if i==0:
+        print(w[i],end="")
+    else:
+        if w[i] == w[0]:
+                print("$",end="")
+        else:
+                    print(w[i],end="")
