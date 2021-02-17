e59d08b89b88e8432b707a52a23446b9936fc419
Adithyabinu <77437806+Adithyabinu@users.noreply.github.com>
2021-02-17 07:57:59 +0530

    Add files via upload

---
+++ Co2programe6.py
@@ -0,0 +1,8 @@
+test_str = "adithya binu"
+count = 0
+  
+for i in test_str: 
+    if i == 'i': 
+        count = count + 1
+print ("Count of e in adithya binu is : "
+                            +  str(count)) 
