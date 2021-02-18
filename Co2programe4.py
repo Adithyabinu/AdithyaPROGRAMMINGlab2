3dd0cf11e5160e8df740dca6b6abd127f044054c
Adithyabinu <77437806+Adithyabinu@users.noreply.github.com>
2021-02-18 12:29:25 +0530

    Add files via upload

---
+++ Co2programe4.py
@@ -0,0 +1,9 @@
+i=int(input('enter the starting range:'))
+j=int(input('enter the ending range:'))
+for i in range(i,j,1):
+    for j in range(32,100,1):
+        if i==j*j:
+           s=str(i)
+           if(int(s[0])%2==0) and (int(s[1])%2==0)and(int(s[2])%2==0)and(int(s[3])%2==0):
+                   print(i)
+ 
